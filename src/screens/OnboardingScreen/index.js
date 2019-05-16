52eaa60c97e94c2f6233c79357937950a79ff2fb
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-16 22:15:11 +0500

    --Commit: Onboarding screen half completed

--- src/screens/OnboardingScreen/index.js
+++ src/screens/OnboardingScreen/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 
-import { View, Image } from 'react-native'
+import { View, Image, Text } from 'react-native'
 import Images from '../../assets/images';
-
+import { FontAwesome } from "@expo/vector-icons";
 export default class OnboardingScreen extends Component {
     constructor() {
         super()
@@ -11,10 +11,19 @@ export default class OnboardingScreen extends Component {
     render() {
         return (
             <View style={{ flex: 1, justifyContent: 'center' }}>
-                <View>
-                    <Image source={Images.Onboarding.main} />
+                <View style={{ alignSelf: 'center' }}>
+                    <Image source={Images.Onboarding.main} style={{ width: 186.17, height: 201 }} />
                 </View>
-
+                <View style={{alignSelf: 'center'}}>
+                    <Text style={{fontSize:22}}>Reminders made simple</Text>
+                    <View style={{flexDirection: 'row', alignSelf: 'center'}}>
+                        <Text style={{fontSize:22}}>Just for You. </Text>
+                        <FontAwesome name="heart" size={30} color="red"/>
+                    </View>
+                </View>
+                <View style={{alignSelf: 'center'}}>
+                    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pellentesque erat in blandit luctus.</Text>
+                </View>
             </View>
         )
     }
