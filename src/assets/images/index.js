6ef218e1994d521c4092ae65c159785d1b3af5b4
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-17 01:06:57 +0500

    --Commit: Images added for Empty Todo

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,11 +1,15 @@
 const Images = {
     Onboarding: {
         main: require('./screens/onboarding/Onboarding.png')
+    },
+    Dashboard: {
+        emptyTodo: require('./screens/dashboard/EmptyTodo.png'),
     }
 }
 
 export const cachedImages = [
-    require('./screens/onboarding/Onboarding.png')
+    require('./screens/onboarding/Onboarding.png'),
+    require('./screens/dashboard/EmptyTodo.png')
 ]
 
 export default Images
