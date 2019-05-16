db20433a825955cc0c55f4cd06b559033e7e54b1
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-17 02:07:29 +0500

    --Commit: No task screen completed

--- src/screens/DashboardScreen/index.js
+++ src/screens/DashboardScreen/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Image, Text, Dimensions, TouchableOpacity } from 'react-native'
+import { View, Image, Text, Dimensions, ImageBackground } from 'react-native'
 import { FontAwesome } from "@expo/vector-icons";
 import Images from '../../assets/images';
 import Colors from '../../constants/Colors'
@@ -11,13 +11,25 @@ export default class DashboardScreen extends Component {
         const { emptyTodo, header } = Images.Dashboard
         return (
             <View style={styles.container}>
-                <Image source={header} style={{ width: width }} />
+                <ImageBackground source={header} style={styles.headerImage}>
+                    <View style={styles.headerContainer}>
+                        <View style={styles.rowView}>
+                            <View style={[{ flex: 1 }, styles.centered]}>
+                                <Text style={styles.greeting}>Hello Brenda!</Text>
+                                <Text style={styles.taskStatus}>Hello Brenda!</Text>
+                            </View>
+                            <FontAwesome name="user-circle-o" size={40} color="white" />
+                        </View>
+
+                    </View>
+                </ImageBackground>
                 <View style={styles.imageContainer}>
                     <View style={styles.centered}>
                         <Image source={emptyTodo} style={styles.topImage} />
                     </View>
-                    <View style={styles.centered}>
+                    <View style={[styles.centered, { marginTop: 75 }]}>
                         <Text style={styles.heading}>No tasks</Text>
+                        <Text style={styles.description}>You have no task to do.</Text>
                     </View>
                 </View>
 
@@ -34,14 +46,48 @@ const styles = {
     centered: {
         alignSelf: 'center'
     },
+    headerContainer: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    description: {
+        fontSize: 17,
+        fontFamily: "opensans-regular",
+        textAlign: 'center',
+        color: Colors.descriptionColor
+    },
+    greeting: {
+        fontFamily: "rubik-regular",
+        fontSize: 17,
+        color: Colors.white,
+    },
     heading: {
         fontSize: 22,
-        fontFamily: "rubik-medium"
+        fontFamily: "rubik-medium",
+        textAlign: 'center',
+        color: Colors.headingColor
+    },
+    headerImage: {
+        flex: 1,
+        width: width,
+        height: 106,
+        paddingHorizontal: 20
     },
     imageContainer: {
-        flex: 1,
+        flex: 3,
         alignContent: 'center',
         marginTop: 100
+    },
+    rowView: {
+        flex: 1,
+        flexDirection: 'row',
+        position: 'absolute',
+        bottom: 50
+    },
+    taskStatus: {
+        fontFamily: "rubik-regular",
+        fontSize: 12,
+        color: Colors.white,
     },
     topImage: {
         width: 119.96,
