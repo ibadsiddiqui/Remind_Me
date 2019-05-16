dee6127f13521287e7190abee0193b22de92434a
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-16 22:06:28 +0500

    --Commit: App images added

---
+++ src/assets/images/index.js
@@ -0,0 +1,11 @@
+const Images = {
+    Onboarding: {
+        main: require('./screens/onboarding/Onboarding.png')
+    }
+}
+
+export const cachedImages = [
+    require('./screens/onboarding/Onboarding.png')
+]
+
+export default Images
