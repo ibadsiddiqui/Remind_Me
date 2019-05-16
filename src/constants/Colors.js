8d7c036fedf965ce814f5b5abb4d3fdaa59fd74b
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-17 02:05:06 +0500

    --Commit: Description and heading color added

--- src/constants/Colors.js
+++ src/constants/Colors.js
@@ -1,7 +1,9 @@
 
 export default {
+  activeTintColor: "#5F87E7",
+  descriptionColor: "#82A0B7",
+  headingColor: "#554E8F",
+  inactiveTintColor: '#9F9F9F',
   onboardingBtnBackground: "#48B413",
   white: '#fff',
-  activeTintColor:"#5F87E7",
-  inactiveTintColor: '#9F9F9F'
 };
