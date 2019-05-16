bf98d2cc5d13086cbca2a4da25c0000f6adaf3f9
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-16 22:40:11 +0500

    --Commit: Screen created for onboarding

--- src/screens/OnboardingScreen/index.js
+++ src/screens/OnboardingScreen/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 
-import { View, Image, Text } from 'react-native'
+import { View, Image, Text, Dimensions, TouchableOpacity } from 'react-native'
 import Images from '../../assets/images';
 import { FontAwesome } from "@expo/vector-icons";
+import Colors from '../../constants/Colors'
+const {width, height} = Dimensions.get('window')
 export default class OnboardingScreen extends Component {
     constructor() {
         super()
@@ -10,21 +12,68 @@ export default class OnboardingScreen extends Component {
 
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center' }}>
-                <View style={{ alignSelf: 'center' }}>
-                    <Image source={Images.Onboarding.main} style={{ width: 186.17, height: 201 }} />
-                </View>
-                <View style={{alignSelf: 'center'}}>
-                    <Text style={{fontSize:22}}>Reminders made simple</Text>
-                    <View style={{flexDirection: 'row', alignSelf: 'center'}}>
-                        <Text style={{fontSize:22}}>Just for You. </Text>
-                        <FontAwesome name="heart" size={30} color="red"/>
+            <View style={styles.container}>
+                <View style={{flex:1, alignContent:'center', marginTop: 100}}>
+                    <View style={styles.centered}>
+                        <Image source={Images.Onboarding.main} style={{ width: 186.17, height: 201 }} />
                     </View>
                 </View>
-                <View style={{alignSelf: 'center'}}>
-                    <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pellentesque erat in blandit luctus.</Text>
+                <View style={styles.centered}>
+                    <Text style={styles.heading}>Reminders made simple</Text>
+                    <View style={[styles.rowView, styles.centered]}>
+                        <Text style={styles.heading}>just for You. </Text>
+                        <FontAwesome name="heart" size={20} color="red" style={{marginTop:2.5}}/>
+                    </View>
                 </View>
+                <View style={styles.descriptionContainer}>
+                    <Text style={styles.descriptionText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pellentesque erat in blandit luctus.</Text>
+                </View>
+                <View style={{alignSelf: 'center',justifyContent: 'center', marginTop: 120}}>
+                    <TouchableOpacity style={styles.getStartedBtn}>
+                        <Text style={styles.getStartedText}>Get Started</Text>
+                    </TouchableOpacity>
+                </View>
+
             </View>
         )
+    }
+}
+
+const styles = {
+    container: { 
+        flex: 1, 
+        justifyContent: 'center' 
+    },
+    centered: { 
+        alignSelf: 'center'
+    },
+    rowView: {
+        flexDirection: 'row', 
+    },
+    heading:{
+        fontSize:22, 
+        fontFamily:"rubik-medium"
+    },
+    descriptionContainer:{
+        alignSelf: 'center', 
+        width: width * 0.75, 
+        marginTop: 20
+    },
+    descriptionText:{
+        textAlign: 'center', 
+        fontFamily:"opensans-regular"
+    },
+    getStartedBtn:{
+        width:258, 
+        height: 52, 
+        backgroundColor: Colors.onboardingBtnBackground, 
+        justifyContent: 'center', borderRadius: 10
+    },
+    getStartedText:{
+        color:'white', 
+        fontSize: 15, 
+        textAlign: 'center', 
+        fontFamily:"opensans-regular", 
+        fontWeight: '700'
     }
 }
