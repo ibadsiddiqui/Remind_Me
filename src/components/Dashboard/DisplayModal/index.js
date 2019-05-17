98e50d6189cef511f6d017d0da78719a1a8738b4
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-18 00:22:22 +0500

    --Commit: Task button component seperated

--- src/components/Dashboard/DisplayModal/index.js
+++ src/components/Dashboard/DisplayModal/index.js
@@ -1,29 +1,44 @@
 import React from 'react'
-import { Modal, View, Text, StyleSheet, TouchableHighlight, Dimensions } from 'react-native';
+import { Modal, Image, View, Text, StyleSheet, TouchableHighlight, Dimensions } from 'react-native';
+import Icon from '@expo/vector-icons/FontAwesome';
+import Images from '../../../assets/images';
+import MainButton from '../../General/MainButton';
 
-const { width, height } = Dimensions.get('window')
+const { width } = Dimensions.get('window')
 const DisplayModal = (props) => {
+  const { triggerModal, modalVisible } = props
   return (
     <Modal
       animationType="slide"
-      transparent={false}
-      visible={props.modalVisible}
-      onRequestClose={() => {
-        Alert.alert('Modal has been closed.');
-      }}>
-      <View style={{ marginTop: 22, height, width }}>
-        <View>
-          <Text>Hello World!</Text>
-
-          <TouchableHighlight
-            onPress={() => {
-              this.setModalVisible(!props.modalVisible);
-            }}>
-            <Text>Hide Modal</Text>
-          </TouchableHighlight>
+      transparent={true}
+      visible={modalVisible}
+      onRequestClose={() => { }}
+    >
+      <View style={{ flex: 1, backgroundColor: 'rgba(0,0,0,0.2)', paddingTop: 100 }}>
+        <View style={{ position: 'absolute', paddingTop: 120 }}>
+          <Image source={Images.modalBackground} style={{ width: width }} resizeMode="cover" />
+        </View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row' }}>
+            <View style={{ flex: 1, alignSelf: 'stretch', alignItems: 'center' }}>
+              <MainButton {...props}
+                triggerModal={() => triggerModal()}
+              />
+            </View>
+          </View>
+        </View>
+        <Text style={{ textAlign: 'center' }}>Add new tasks</Text>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row' }}>
+            <TouchableHighlight
+              onPress={() => triggerModal()}>
+              <Text>Hide Modal</Text>
+            </TouchableHighlight>
+          </View>
+          {/* </View> */}
         </View>
       </View>
-    </Modal>
+    </Modal >
   )
 }
 const styles = StyleSheet.create({
---
+++ src/components/General/MainButton/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { TouchableHighlight, View } from "react-native";
+import Icon from '@expo/vector-icons/FontAwesome';
+import { Entypo } from '@expo/vector-icons';
+import Colors from '../../../constants/Colors';
+// import DisplayModal from './../components/Dashboard/DisplayModal';
+const SIZE = 80;
+
+const MainButton = (props) => {
+    const { triggerModal, modalVisible } = props;
+    console.log(modalVisible);
+
+    if (modalVisible === false)
+        return (
+            <TouchableHighlight
+                onPress={() => triggerModal()}
+                style={styles.button}
+            >
+                <Icon name="plus" size={24} color="white" />
+            </TouchableHighlight>
+        )
+    else if (modalVisible === true)
+        return (
+            modalVisible === true &&
+            <TouchableHighlight
+                onPress={() => triggerModal()}
+                style={styles.button}
+            >
+                <Entypo name="cross" size={30} color="white" />
+            </TouchableHighlight>
+        )
+}
+
+const styles = {
+    container: {
+        position: 'absolute',
+        alignItems: 'center'
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: SIZE - 20,
+        height: SIZE - 20,
+        borderRadius: SIZE / 2,
+        backgroundColor: Colors.taskButton,
+        marginBottom: 50,
+        elevation: 10
+    }
+}
+
+export default MainButton;
