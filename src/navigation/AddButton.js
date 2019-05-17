6f249ac401b75c6d4b06dfd470a445c214a32868
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-17 22:26:03 +0500

    --Commit : modal toggler added

--- src/navigation/AddButton.js
+++ src/navigation/AddButton.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import { TouchableHighlight, View } from "react-native";
 import Icon from '@expo/vector-icons/FontAwesome';
-
+import DisplayModal from './../components/Dashboard/DisplayModal';
 const SIZE = 80;
 
-class AddButton extends Component {
+class AddButton extends Component { 
+    state = {
+        modalVisible: false
+    }
+
+    triggerModal = () => {
+        this.setState(prevState => ({
+            modalVisible: !prevState.modalVisible
+        }));
+      }
+    
     render() {
+        console.log(this.state)
         return (
             <View style={{
                 position: 'absolute',
                 alignItems: 'center'
             }}>
                 <TouchableHighlight
-                    onPress={this.toggleView}
+                    onPress={() => this.triggerModal()}
                     underlayColor="#2882D8"
                     style={{
                         alignItems: 'center',
@@ -27,6 +38,10 @@ class AddButton extends Component {
                 >
                     <Icon name="plus" size={24} color="#F8F8F8" />
                 </TouchableHighlight>
+                <DisplayModal   
+                    data = "Krunal"
+                    modalVisible = { this.state.modalVisible }
+                />
             </View>
         );
     }
