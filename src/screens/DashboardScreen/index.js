8cc10639c3554f1981e01594b294c253acd499c5
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-17 21:50:17 +0500

    --Coomit: unneccesary import removed

--- src/screens/DashboardScreen/index.js
+++ src/screens/DashboardScreen/index.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import Images from '../../assets/images';
 import NoTask from './../../components/Dashboard/NoTask';
 import Header from './../../components/Dashboard/Header';
 
 export default class DashboardScreen extends Component {
-
     render() {
-        const { emptyTodo, header } = Images.Dashboard;
         return (
             <View style={styles.container}>
                 <Header />
