99d0e2eb7794db47fa894946ff745b7461e271ee
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-17 15:03:25 +0500

    --Commit: Localization added

---
+++ src/constants/Locale.js
@@ -0,0 +1,9 @@
+const Locale = {
+    Onboarding:{
+        title1:"Reminders made simple",
+        title2:"just for You.",
+        Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pellentesque erat in blandit luctus."
+    }
+}
+
+export default Locale;
