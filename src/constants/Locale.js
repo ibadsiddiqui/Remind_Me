e13c110ac73b23811db3b0f47039403337735a55
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-17 15:35:44 +0500

    --Commit: Task Locale added

--- src/constants/Locale.js
+++ src/constants/Locale.js
@@ -1,8 +1,13 @@
 const Locale = {
-    Onboarding:{
-        title1:"Reminders made simple",
-        title2:"just for You.",
+    Onboarding: {
+        title1: "Reminders made simple",
+        title2: "just for You.",
         Desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pellentesque erat in blandit luctus."
+    },
+    Tasks: {
+        NoTasksDesc: "You have no task to do.",
+        NoTasksHeading: "No tasks",
+        NoTaskNotification: "Today you have no tasks!",
     }
 }
 
