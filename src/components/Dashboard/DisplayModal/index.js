92c126bcc8e47568d5d6715254f8bfc5067f3fc8
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-19 17:47:01 +0500

    --Commit: New Task Modal code refactored

--- src/components/Dashboard/DisplayModal/index.js
+++ src/components/Dashboard/DisplayModal/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import { Modal, Image, View, Text, Dimensions, TextInput, ScrollView, TouchableOpacity } from 'react-native';
+import { Modal, Image, View, Text, Dimensions } from 'react-native';
 import Images from '../../../assets/images';
-import MainButton from '../../General/MainButton';
-// import {  } from 'react-native-gesture-handler';
 
-const { width, height } = Dimensions.get('window')
+import ListOfFlag from '../../General/ListofTag';
+import UserInput from './UserInput';
+import { ModalHeader, ModalHeading } from './ModalHeader';
+
+const { width } = Dimensions.get('window')
+
 const DisplayModal = (props) => {
-  const { triggerModal, modalVisible } = props
+  const { modalVisible } = props
   return (
     <Modal
       animationType="slide"
@@ -19,69 +22,12 @@ const DisplayModal = (props) => {
           <Image source={Images.modalBackground} style={{ width: width }} resizeMode="cover" />
         </View>
         <View style={styles.tableContainer}>
-          <View style={styles.cancelBtnRowContainer}>
-            <View style={styles.cancelBtnContainer}>
-              <MainButton {...props} triggerModal={() => triggerModal()} />
-            </View>
-          </View>
-          <View style={styles.headingRowContainer}>
-            <View style={styles.headingContainer}>
-              <Text style={styles.addTaskHeading}>Add new tasks</Text>
-            </View>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'flex-start', flexDirection: 'row' }}>
-            <View style={{ flex: 1, alignSelf: 'flex-start', alignItems: 'center' }}>
-              <TextInput style={{
-                width: width * 0.95, height: height * 0.1, paddingBottom: 20, paddingHorizontal: 5,
-                textDecorationLine: "none", fontSize: 32, fontFamily: "rubik-regular",
-                borderWidth: 1,
-                borderColor: "transparent",
-                borderBottomColor: "#CFCFCF"
-              }}
-                placeholder="Enter task here"
-              />
-            </View>
-          </View>
+          <ModalHeader {...props} />
+          <ModalHeading />
+          <UserInput />
           <View style={{ flex: 0.5, alignSelf: 'stretch', flexDirection: 'row', }}>
             <View style={{ flex: 1, alignSelf: 'center', alignItems: 'center' }}>
-              <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}
-                style={{
-                  width: width * 0.95,
-                  borderWidth: 1,
-                  borderColor: "transparent",
-                  borderBottomColor: "#CFCFCF"
-                }}>
-                <TouchableOpacity >
-                  <View style={[styles.reminderTypeContainer, { marginLeft: 0 }]}>
-                    <View style={{ width: 20, height: 20, borderRadius: 20, backgroundColor: '#FFD506', marginHorizontal: 5 }} />
-                    <Text style={styles.reminderTypeText}>Personal</Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity >
-                  <View style={styles.reminderTypeContainer}>
-                    <View style={{ width: 20, height: 20, borderRadius: 20, backgroundColor: '#5DE61A', marginHorizontal: 5 }} />
-                    <Text style={styles.reminderTypeText}>Work</Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <View style={styles.reminderTypeContainer}>
-                    <View style={{ width: 20, height: 20, borderRadius: 20, backgroundColor: '#D10263', marginHorizontal: 5 }} />
-                    <Text style={styles.reminderTypeText}>Meeting</Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <View style={styles.reminderTypeContainer}>
-                    <View style={{ width: 20, height: 20, borderRadius: 20, backgroundColor: '#3044F2', marginHorizontal: 5 }} />
-                    <Text style={styles.reminderTypeText}>Study</Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <View style={styles.reminderTypeContainer}>
-                    <View style={{ width: 20, height: 20, borderRadius: 20, backgroundColor: '#F29130', marginHorizontal: 5 }} />
-                    <Text style={styles.reminderTypeText}>Shopping</Text>
-                  </View>
-                </TouchableOpacity>
-              </ScrollView>
+              <ListOfFlag />
             </View>
           </View>
           <View style={{ flex: 3, alignSelf: 'stretch', flexDirection: 'row' }} />
@@ -91,11 +37,6 @@ const DisplayModal = (props) => {
   )
 }
 const styles = {
-  addTaskHeading: {
-    fontSize: 13,
-    fontFamily: "rubik-medium",
-    textAlign: 'center'
-  },
   container: {
     flex: 1,
     backgroundColor: 'rgba(0,0,0,0.2)',
@@ -110,40 +51,6 @@ const styles = {
     alignItems: 'center',
     justifyContent: 'center'
   },
-  cancelBtnRowContainer: {
-    flex: 0.75,
-    alignSelf: 'stretch',
-    flexDirection: 'row'
-  },
-  cancelBtnContainer: {
-    flex: 1,
-    alignSelf: 'center',
-    alignItems: 'center'
-  },
-  headingRowContainer: {
-    flex: 0.25,
-    alignSelf: 'flex-start',
-    flexDirection: 'row'
-  },
-  headingContainer: {
-    flex: 1,
-    alignSelf: 'flex-start',
-    alignItems: 'center'
-  },
-
-  reminderTypeContainer: {
-    marginHorizontal: 10,
-    flexDirection: 'row',
-  },
-  reminderTypeText: {
-    fontFamily: "rubik-regular",
-    fontSize: 16,
-    color: "#8E8E8E"
-  },
-  text: {
-    fontSize: 20,
-    marginLeft: 150
-  }
 }
 
 export default DisplayModal;
