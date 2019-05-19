cb8cd3eac205536f95b3d1d8add7891fa3b20743
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-19 17:45:37 +0500

    --Commit: User input created for modal

---
+++ src/components/Dashboard/DisplayModal/UserInput.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { View, Dimensions, TextInput } from 'react-native';
+
+const { width, height } = Dimensions.get('window')
+const UserInput = (props) => {
+    return (
+        <View style={styles.container}>
+            <View style={styles.textInputContainer}>
+                <TextInput style={styles.textInput}
+                    placeholder="Enter task here"
+                />
+            </View>
+        </View>
+    )
+}
+const styles = {
+    container: {
+        flex: 1,
+        alignSelf: 'flex-start',
+        flexDirection: 'row'
+    },
+    textInputContainer: {
+        flex: 1,
+        alignSelf: 'flex-start',
+        alignItems: 'center'
+    },
+    textInput: {
+        width: width * 0.95,
+        height: height * 0.1,
+        paddingBottom: 20,
+        paddingHorizontal: 5,
+        textDecorationLine: "none",
+        fontSize: 32,
+        fontFamily: "rubik-regular",
+        borderWidth: 1,
+        borderColor: "transparent",
+        borderBottomColor: "#CFCFCF"
+    },
+}
+
+export default UserInput;
