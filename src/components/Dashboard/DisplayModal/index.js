1dcf6805260a13cfa734afe8e78e6c6a53d9ac93
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-20 14:15:34 +0500

    --Commit: Time and date added

--- src/components/Dashboard/DisplayModal/index.js
+++ src/components/Dashboard/DisplayModal/index.js
@@ -31,25 +31,31 @@ const DisplayModal = (props) => {
               <ListOfFlag />
             </View>
           </View>
-          <View style={{ flex: 3, alignSelf: 'stretch', flexDirection: 'row', }}>
+          <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', }}>
             <View style={{
-              flex: 3,
+              flex: 1,
               alignSelf: 'center',
               alignItems: 'flex-start',
               paddingHorizontal: 10,
               marginTop: 40
             }}>
-              <TouchableHighlight >
+              <TouchableHighlight>
                 <View style={{ flex: 3, flexDirection: 'row' }}>
                   <Text style={{ textAlign: 'center', fontFamily: "rubik-regular", fontSize: 13 }}>Choose date</Text>
                   <Entypo
                     name="chevron-small-down"
                     size={20} color="black"
-                    style={{ marginLeft: 75, marginBottom: 5 }} />
+                    style={{ marginLeft: 75, marginBottom: 10 }}
+                  />
                 </View>
               </TouchableHighlight>
             </View>
           </View>
+          <View style={{ flex: 1, paddingHorizontal: 10, alignSelf: 'flex-start', flexDirection: 'row', alignItems: 'flex-start' }}>
+            <Text style={{ fontFamily: "rubik-medium", fontSize: 13, textAlign: 'center', marginTop: 2 }}> Today, 19:00 — 21:00 </Text>
+            <Entypo name="chevron-down" size={20} color="black" />
+          </View>
+          <View style={{ flex: 2 }} />
         </View>
       </View>
     </Modal >
