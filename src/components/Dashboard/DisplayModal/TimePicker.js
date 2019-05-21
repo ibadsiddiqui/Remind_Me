7e8e10aa314f73ec889a788cad6c59185cccc3be
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-21 14:03:59 +0500

    --commit: Time picker component added

---
+++ src/components/Dashboard/DisplayModal/TimePicker.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {  View, Text } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+
+
+
+const TimePicker = (props) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.heading}>Time: </Text>
+            <Text style={styles.time}>19:00 — 21:00 </Text>
+            <Entypo
+                name="chevron-down"
+                size={20} color="black"
+                style={styles.icon} />
+        </View>
+    )
+}
+const styles = {
+    container: {
+        flex: 1,
+        paddingHorizontal: 10,
+        alignSelf: 'flex-start',
+        flexDirection: 'row',
+        alignItems: 'flex-start'
+    },
+    heading: {
+        fontFamily: "rubik-medium",
+        fontSize: 13,
+        textAlign: 'center',
+        marginTop: 2
+    },
+    time: {
+        fontFamily: "rubik-medium",
+        fontSize: 13,
+        textAlign: 'center',
+        marginTop: 2,
+        marginLeft: 25
+    },
+    icon: {
+        marginLeft: 15,
+        top: -2
+    }
+}
+
+export default TimePicker;
