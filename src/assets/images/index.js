076834b7b0fe20139c5aaf0204667ae6a0c55493
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-05-23 20:04:29 +0500

    --Commit: Assets cached

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -7,13 +7,28 @@ const Images = {
         header: require('./screens/dashboard/Header.png'),
     },
     modalBackground: require('./screens/tabs/AddBar.png'),
+    listOfTasks: {
+        personal: require('./screens/task/user.png'),
+        work: require('./screens/task/briefcase.png'),
+        meeting: require('./screens/task/presentation.png'),
+        shopping: require('./screens/task/shopping-basket.png'),
+        party: require('./screens/task/confetti.png'),
+        study: require('./screens/task/molecule.png'),
+
+    }
 }
 
 export const cachedImages = [
     require('./screens/onboarding/Onboarding.png'),
     require('./screens/dashboard/EmptyTodo.png'),
     require('./screens/dashboard/Header.png'),
-    require('./screens/tabs/AddBar.png')
+    require('./screens/tabs/AddBar.png'),
+    require('./screens/task/presentation.png'),
+    require('./screens/task/shopping-basket.png'),
+    require('./screens/task/user.png'),
+    require('./screens/task/briefcase.png'),
+    require('./screens/task/confetti.png'),
+    require('./screens/task/molecule.png'),
 ]
 
 export default Images
