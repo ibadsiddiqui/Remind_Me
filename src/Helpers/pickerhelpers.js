7193bdc51dfffb3876f7766f71b3fd9cae9e3d51
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-07-23 23:15:27 +0500

    --Commit: task date set

--- src/Helpers/pickerhelpers.js
+++ src/Helpers/pickerhelpers.js
@@ -1,13 +1,23 @@
 import { DatePickerAndroid, TimePickerAndroid } from 'react-native'
+import moment from 'moment';
 
-export async function toggleDatePicker() {
+export async function toggleDatePicker(props) {
+    const { setTaskDate } = props;
     try {
         const { action, year, month, day } = await DatePickerAndroid.open({
             date: new Date(),
         });
-        if (action !== DatePickerAndroid.dismissedAction) {
-            // Selected year, month (0-11), day
+        if (action !== DatePickerAndroid.dismissedAction) { // Selected year, month (0-11), day
+            if (month + 1 < 10)
+                setTaskDate(moment(moment(`${year}0${month + 1}${day}`, "YYYYMMDD")).calendar())
+            else
+                setTaskDate(moment(moment(`${year}${month + 1}${day}`, "YYYYMMDD")).calendar())
         }
+        if (month + 1 < 10)
+            setTaskDate(moment(moment(`${year}0${month + 1}${day}`, "YYYYMMDD")).calendar())
+        else
+            setTaskDate(moment(moment(`${year}${month + 1}${day}`, "YYYYMMDD")).calendar())
+
     } catch ({ code, message }) {
         console.warn('Cannot open date picker', message);
     }
