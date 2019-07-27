132b34c9713b6492ffebcf5f61ba938144e7bb7b
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-07-27 08:25:33 +0500

    --Commit: types constants updated

--- src/constants/Types.js
+++ src/constants/Types.js
@@ -1,7 +1,10 @@
-
+// fill Details flag
 export const SET_TASK_DESCRIPTION = "SET_TASK_DESCRIPTION";
 export const SET_TASK_FLAG = "SET_TASK_FLAG";
 export const SET_TASK_DATE = "SET_TASK_DATE";
-export const SET_TASK_TIME = 'SET_TASK_TIME';
+export const SET_TASK_END_TIME = 'SET_TASK_END_TIME';
+export const SET_TASK_START_TIME = 'SET_TASK_START_TIME';
+
+
 export const CREATE_TASK = "CREATE_TASK";
 export const SET_TASK_LIST = "SET_TASK_LIST";
