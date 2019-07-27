5aeff71e5d6aecb262363c7610f9f7276c059fc0
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-07-27 22:26:30 +0500

    --Commit: unneccesary code removed

--- src/Helpers/timeConverter.js
+++ src/Helpers/timeConverter.js
@@ -8,16 +8,3 @@ export function hour24Converter(hour, minute) {
     }
     return time; // return adjusted time or original string
 }
-
-export function tConvert(time) {
-    // Check correct time format and split into components
-    time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
-    if (time.length > 1) { // If time format correct
-        time = time.slice(1);  // Remove full string match value
-        time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
-        if ((time[0] % 12 || 12) > 9)
-            time[0] = +time[0] % 12 || 12; // Adjust hours
-        else time[0] = "0" + time[0] % 12 || 12;
-    }
-    return time.join(''); // return adjusted time or original string
-}
