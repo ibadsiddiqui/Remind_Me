67feb7bc328abaf6cd562d57cc446514d65e2d77
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-07-27 08:24:08 +0500

    --Commit: Start time and end time added

--- src/components/Dashboard/DisplayModal/TimePicker.js
+++ src/components/Dashboard/DisplayModal/TimePicker.js
@@ -2,23 +2,40 @@ import React from 'react'
 import { View, Text, TouchableHighlight } from 'react-native';
 import Entypo from '@expo/vector-icons/Entypo';
 import { toggleTimePicker } from '../../../helpers/pickerhelpers';
-
-
+import Layout from '../../../constants/Layout';
 
 const TimePicker = (props) => {
+    const { } = props;
     return (
-        <View style={styles.container}>
-            <Text style={styles.heading}>Time: </Text>
-            <TouchableHighlight underlayColor="transparent" onPress={() => toggleTimePicker()}>
-                <Text style={styles.time}>19:00 — 21:00 </Text>
+        <View style={[Layout.tableRow, { paddingHorizontal: 10 }]}>
+            <View style={[Layout.tableCell,{flex:1.5}]}>
+                <Text style={styles.heading}>Start Time: </Text>
+            </View>
+            <View style={[Layout.tableCell, { flex:1.35}]}>
+                <TouchableHighlight underlayColor="transparent" onPress={() => toggleTimePicker("start",props)}>
+                    <Text style={styles.time}>00:00 </Text>
+                </TouchableHighlight>
+            </View>
+            <TouchableHighlight style={[Layout.tableCell, 0.1]} underlayColor="transparent" onPress={() => toggleTimePicker(props)}>
+                <Entypo name="chevron-down" size={20} color="black"
+                    style={styles.icon} />
             </TouchableHighlight>
-            <Entypo
-                name="chevron-down"
-                size={20} color="black"
-                style={styles.icon} />
+            <View style={[Layout.tableCell,{flex:1.5}]}>
+                {/* <Text style={styles.heading}>End Time: </Text> */}
+            </View>
+            <View style={[Layout.tableCell, { flex:1.35}]}>
+                {/* <TouchableHighlight underlayColor="transparent" onPress={() =>{}}> */}
+                    {/* <Text style={styles.time}>19:00 </Text> */}
+                {/* </TouchableHighlight> */}
+            </View>
+            <View style={[Layout.tableCell, 0.1]} >
+                {/* <Entypo name="chevron-down" size={20} color="black"
+                    style={styles.icon} /> */}
+            </View>
         </View>
     )
 }
+
 const styles = {
     container: {
         flex: 1,
