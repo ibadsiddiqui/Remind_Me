137fa2beba0e2f1929ca25261c5a137e14fcb00c
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-07-28 03:42:03 +0500

    --Commit: conditional rendering of list added

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import NoTask from '../../components/Home/NoTask';
 import Header from '../../components/Home/Header';
 import { connect } from 'react-redux'
@@ -7,10 +7,23 @@ import { mapStateToProps, mapDispatchToProps } from '../../redux/dispatchers';
 
 class HomeScreen extends Component {
     render() {
+
         return (
             <View style={styles.container}>
                 <Header />
-                <NoTask />
+                {
+                    this.props.TaskList.length === 0 ?
+                        <NoTask />
+                        :
+                        <FlatList
+                            data={this.props.TaskList}
+                            renderItem={({ item, index }) =>
+                                <View style={{flex:1}}>
+                                    <Text key={index}>{item.taskDescription}</Text>
+                                </View>
+                            }
+                        />
+                }
             </View>
         )
     }
