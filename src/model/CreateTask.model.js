5b89d8807b1f58873eee75606fdef3f720bcf0fc
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-07-27 08:25:20 +0500

    --Commit: model updated for start and end time

--- src/model/CreateTask.model.js
+++ src/model/CreateTask.model.js
@@ -4,5 +4,6 @@ export const TaskCreationObject = {
     taskDescription: "",
     taskFlag: "",
     taskDate: moment().calendar(),
-    taskTime: "",
+    taskStartTime: "",
+    taskEndTime: "",
 };
