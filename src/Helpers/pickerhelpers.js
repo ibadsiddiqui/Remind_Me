1428481ff764df3877999c483392bdb55c6a38ab
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-07-28 12:33:05 +0500

    --Commit: updated

--- src/Helpers/pickerhelpers.js
+++ src/Helpers/pickerhelpers.js
@@ -1,5 +1,4 @@
 import { DatePickerAndroid, TimePickerAndroid } from 'react-native'
-import moment from 'moment';
 import { hour24Converter } from './timeConverter';
 import { checkDateAndSlice } from './stringHelper';
 
--- src/Helpers/stringHelper.js
+++ src/Helpers/stringHelper.js
@@ -2,14 +2,8 @@ import moment from "moment";
 
 export function checkDateAndSlice(year, month, day) {
     if (moment(moment(`${year}0${month + 1}${day}`, "YYYYMMDD")).calendar().includes(" at ")) {
-        if (month + 1 < 10) {
-            let date = moment(moment(`${year}0${month + 1}${day}`, "YYYYMMDD")).calendar()
-            return date.slice(0, date.indexOf(" at "))
-        }
-        else {
-            let date = moment(moment(`${year}${month + 1}${day}`, "YYYYMMDD")).calendar()
-            return date.slice(0, date.indexOf(" at "))
-        }
+        if (month + 1 < 10) return moment(moment(`${year}0${month + 1}${day}`, "YYYYMMDD")).calendar();
+        else return moment(moment(`${year}${month + 1}${day}`, "YYYYMMDD")).calendar();
     } else {
         if (month + 1 < 10) return moment(moment(`${year}0${month + 1}${day}`, "YYYYMMDD")).calendar();
         else return moment(moment(`${year}${month + 1}${day}`, "YYYYMMDD")).calendar();
