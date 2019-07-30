24db551c46638f456e2369ab85befcb73f5a93a5
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-07-30 12:08:49 +0500

    --Commit: date helper and day comparer added

--- src/Helpers/stringHelper.js
+++ src/Helpers/stringHelper.js
@@ -2,10 +2,18 @@ import moment from "moment";
 
 export function checkDateAndSlice(year, month, day) {
     if (moment(moment(`${year}0${month + 1}${day}`, "YYYYMMDD")).calendar().includes(" at ")) {
-        if (month + 1 < 10) return moment(moment(`${year}0${month + 1}${day}`, "YYYYMMDD")).calendar();
-        else return moment(moment(`${year}${month + 1}${day}`, "YYYYMMDD")).calendar();
+        if (month + 1 < 10) return new Date(`${year}/0${month + 1}/${day}`).toLocaleString();
+        else return new Date(`${year}/${month + 1}/${day}`).toLocaleString();
     } else {
-        if (month + 1 < 10) return moment(moment(`${year}0${month + 1}${day}`, "YYYYMMDD")).calendar();
-        else return moment(moment(`${year}${month + 1}${day}`, "YYYYMMDD")).calendar();
+        if (month + 1 < 10) return new Date(`${year}/0${month + 1}/${day}`).toLocaleString();
+        else return new Date(`${year}/${month + 1}/${day}`).toLocaleString();
     }
+}
+
+export function comparingWithAt(item1, item2) {
+    return item1 !== moment(item2.taskDate).calendar().slice(0, moment(item2.taskDate).calendar().indexOf(" at "))
+}
+
+export function comparingWithOutAt(item1, item2) {
+    return item1 !== moment(item2.taskDate).calendar();
 }
