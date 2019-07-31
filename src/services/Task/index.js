0ce18f6ba6377794bd3d25b43f2a852bcde84c12
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-07-31 22:55:35 +0500

    --Commit: task details reset on toggle

--- src/services/Task/index.js
+++ src/services/Task/index.js
@@ -1,16 +1,16 @@
 import moment from "moment";
-import { replaceTaskTimeWithStartTime } from "../../helpers/timeConverter";
+import { getRandomID } from "../../helpers/stringHelper";
 export default class TaskServices {
     constructor() { }
 
     static async CreateTask(props) {
-        const { taskDescription, taskFlag, taskDate, taskStartTime, taskEndTime, createTask, addTaskDays, triggerModal } = props;
-        await replaceTaskTimeWithStartTime(props)
+        const { taskID, taskDescription, taskFlag, taskDate, taskStartTime, taskEndTime, createTask, addTaskDays, triggerModal } = props;
         let date = moment(taskDate).calendar();
         date = date.includes(" at ") ? date.slice(0, date.indexOf(" at ")) : date;
         if (taskDescription) {
             addTaskDays(date);
             await createTask({
+                taskID,
                 taskDescription,
                 taskFlag,
                 taskDate,
@@ -23,9 +23,6 @@ export default class TaskServices {
     }
 
     static ResetTaskDetails(props) {
-        props.setTaskDescription("");
-        props.setTaskFlag("");
-        props.setTaskDate(new Date().toLocaleString());
-        props.setTaskStartTime("00:00 A.M");
+        props.resetCreateTaskDetails();
     }
 }
