03a0ac0ef5129359e9777ec65df63966a52e5367
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-04 01:38:45 +0500

    --Commit: Swipeable row created with delete icon

--- src/components/General/TaskListItems/index.js
+++ src/components/General/TaskListItems/index.js
@@ -4,22 +4,32 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import Colors from '../../../constants/Colors';
 import Layout from '../../../constants/Layout';
 import styles from './styles';
+import AppleStyleSwipeableRow from '../SwipeableRow';
 
 const TaskListItems = (props) => {
-    const { item } = props
+    const { item, toggleTaskStatus } = props
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={[Layout.tableRow, styles.styledRow(item.taskFlag)]}>
-                <View style={styles.iconContainer}>
-                    <MaterialIcons name="radio-button-unchecked" color={Colors.uncheckedCirlce} size={20} />
-                </View>
-                <View style={styles.timeDateContainer}>
-                    <Text style={styles.timeDateTxt}> {item.taskStartTime}</Text>
-                </View>
-                <View style={[Layout.tableCell, { justifyContent: 'center' }]}>
-                    <Text style={styles.headingTxt}>{item.taskDescription}</Text>
-                </View>
-            </TouchableOpacity>
+            <AppleStyleSwipeableRow>
+                <TouchableOpacity style={[Layout.tableRow, styles.styledRow(item.taskFlag)]}
+                    onPress={() => toggleTaskStatus(item.taskID)}
+                >
+                    <View style={styles.iconContainer}>
+                        {
+                            item.completed ?
+                                <MaterialIcons name="radio-button-checked" color={Colors.onboardingBtnBackground} size={20} />
+                                :
+                                <MaterialIcons name="radio-button-unchecked" color={Colors.uncheckedCirlce} size={20} />
+                        }
+                    </View>
+                    <View style={styles.timeDateContainer}>
+                        <Text style={styles.timeDateTxt}> {item.taskStartTime}</Text>
+                    </View>
+                    <View style={[Layout.tableCell, { justifyContent: 'center' }]}>
+                        <Text style={styles.headingTxt(item.completed)}>{item.taskDescription}</Text>
+                    </View>
+                </TouchableOpacity>
+            </AppleStyleSwipeableRow>
         </View>
     )
 }
