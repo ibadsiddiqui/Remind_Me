8c52069df1f08b1ed8e1c88839a2b807701ca296
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-03 23:36:32 +0500

    --Commit: array sorted on render

--- src/components/General/TaskList/index.js
+++ src/components/General/TaskList/index.js
@@ -3,9 +3,9 @@ import { View, Text, FlatList } from 'react-native';
 import TaskListItems from '../TaskListItems';
 import { comparingWithOutAt, comparingWithAt } from '../../../helpers/stringHelper';
 import styles from './styles';
+import { sortArrayAccordingToTime, slicingMomentDateUsingAt } from '../../../helpers/timeConverter';
 
 export default class TaskListGenerator extends React.PureComponent {
-
     _keyExtractor = (item, index) => index.toString();
 
     render() {
@@ -16,9 +16,9 @@ export default class TaskListGenerator extends React.PureComponent {
                 data={[...ListOfDaysSelected]}
                 renderItem={({ item, index }) =>
                     <View style={styles.dayHeadingContainer} key={index.toString()}>
-                        <Text style={styles.dayHeadingText}> {item}</Text>
+                        <Text style={styles.dayHeadingText}> {slicingMomentDateUsingAt(item)}</Text>
                         {
-                            TaskList.map((_item, _index) => {
+                            sortArrayAccordingToTime(TaskList).map((_item, _index) => {
                                 const conditionOne = comparingWithAt(item, _item);
                                 const conditionTwo = comparingWithOutAt(item, _item);
                                 if (conditionOne && conditionTwo)
