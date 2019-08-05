ffd8afe89b203b31291b79c67b0fe75a9c3360b4
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-06 00:13:18 +0500

    --Commit: Collection helper function added

---
+++ src/Helpers/listHelpers.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Tab from "../components/Categories/GridView/Tab";
+
+export function tabMappers(list, props) {
+    return list.map((item, key) => <Tab key={key} item={item} {...props} />)
+}
+
+export function filterListWithFlags(list, flag) {
+    return list.filter((item) => item.taskFlag === flag);
+}
+
+export function sortArrayAccordingToTime(array) {
+    return array.sort((a, b) => new Date(a.taskDate) - new Date(b.taskDate))
+}
