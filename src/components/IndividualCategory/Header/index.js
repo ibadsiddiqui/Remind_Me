e5dec021de01a584ae8006f1ad86a69713e71cce
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-07 01:55:00 +0500

    --Commit: count filter added in individual category header

--- src/components/IndividualCategory/Header/index.js
+++ src/components/IndividualCategory/Header/index.js
@@ -5,18 +5,19 @@ import Images from '../../../assets/images';
 import Colors from '../../../constants/Colors';
 import Locale from '../../../constants/Locale';
 import Layout from '../../../constants/Layout';
-import { filterListWithFlags } from '../../../helpers/listHelpers';
+import { filterListWithFlags, filterTodaysTask } from '../../../helpers/listHelpers';
 const { width } = Dimensions.get('window');
 
 const IndividualHeader = (props) => {
     const { header } = Images.Dashboard;
     const { TaskList, flagType } = props
+    let list = TaskList.length === 0 ? [] : filterTodaysTask(TaskList).length == 0 ? [] : filterTodaysTask(TaskList)[0].data;
     return (
         <View style={[Layout.tableRow]}>
             <Image source={header} style={{ width, position: 'absolute' }} />
             <View style={[Layout.tableCell, { paddingHorizontal: 10, marginTop: width * 0.12 }]}>
                 <Text style={styles.greeting}>{flagType} Tasks</Text>
-                <Text style={styles.taskStatus}>{Locale.Tasks.TaskNotification(filterListWithFlags(TaskList, flagType))}</Text>
+                <Text style={styles.taskStatus}>{Locale.Tasks.TaskNotification(filterListWithFlags(list, flagType))}</Text>
             </View>
             <View style={[Layout.tableCell, styles.userIconContainer]}>
                 <FontAwesome name="user-circle-o" size={40} color="white" />
