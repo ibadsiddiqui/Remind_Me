dec7d9fd128d33e82d3eaaa025bb0709f5b7a571
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-07 01:20:32 +0500

    --Commit: task service updated

--- src/services/Task/index.js
+++ src/services/Task/index.js
@@ -2,17 +2,32 @@ export default class TaskServices {
     constructor() { }
 
     static async CreateTask(props) {
-        const { taskID, taskDescription, taskFlag, taskDate, taskStartTime, taskEndTime, createTask, addTaskDays, triggerModal } = props;
-        if (taskDescription) {
-            addTaskDays(new Date(taskDate).toLocaleDateString());
-            await createTask({
-                taskID,
-                taskDescription,
-                taskFlag,
-                taskDate,
-                taskStartTime,
-                taskEndTime,
-            });
+        const { TaskList, taskDescription, taskFlag, triggerModal } = props;
+        if (taskDescription && taskFlag) {
+            const { addTaskToDate,  createTask, taskID, taskDescription, taskFlag, taskDate, taskStartTime, taskEndTime } = props;
+            let isNewDate = typeof TaskList.find((item) => item.date === new Date(taskDate).toLocaleDateString()) === "undefined" ? true : false
+            if (TaskList.length === 0 || isNewDate) {
+                await createTask({
+                    date: new Date(taskDate).toLocaleDateString(),
+                    data: [{
+                        taskID,
+                        taskDescription,
+                        taskFlag,
+                        taskDate,
+                        taskStartTime,
+                        taskEndTime,
+                    }]
+                });
+            } else {
+                addTaskToDate(new Date(taskDate).toLocaleDateString(), {
+                    taskID,
+                    taskDescription,
+                    taskFlag,
+                    taskDate,
+                    taskStartTime,
+                    taskEndTime,
+                })
+            }
         }
         triggerModal(); // to toggle the create task modal
         TaskServices.ResetTaskDetails(props);
