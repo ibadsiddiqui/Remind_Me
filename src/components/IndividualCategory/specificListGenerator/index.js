5424a099366542fc93e2d1c8913d03f978296c57
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-07 01:55:24 +0500

    --Commit: list updated

--- src/components/IndividualCategory/specificListGenerator/index.js
+++ src/components/IndividualCategory/specificListGenerator/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { FlatList, Text, View } from 'react-native';
-import { filterListWithFlags, sortArrayAccordingToTime } from '../../../helpers/listHelpers';
-import { comparingWithAt, comparingWithOutAt } from '../../../helpers/stringHelper';
 import { slicingMomentDateUsingAt } from '../../../helpers/timeConverter';
 import TaskListItems from '../../General/TaskListItems';
 import styles from './styles';
@@ -11,29 +9,23 @@ export default class SpecificListGenerator extends React.PureComponent {
     _keyExtractor = (item, index) => index.toString();
 
     render() {
-        const { TaskList, ListOfDaysSelected, flagType } = this.props;
-        let sortedArray = sortArrayAccordingToTime(filterListWithFlags(TaskList, flagType));
+        const { TaskList } = this.props;
         return (
             <FlatList style={styles.listContainer}
                 keyExtractor={this._keyExtractor}
-                data={[...ListOfDaysSelected]}
+                data={TaskList}
                 renderItem={({ item, index }) =>
                     <View style={styles.dayHeadingContainer} key={index.toString()}>
-                        <Text style={styles.dayHeadingText}> {slicingMomentDateUsingAt(item)}</Text>
+                        <Text style={styles.dayHeadingText}> {slicingMomentDateUsingAt(item.date)}</Text>
                         {
-                            sortedArray.map((_item, _index) => {
-                                const conditionOne = comparingWithAt(item, _item);
-                                const conditionTwo = comparingWithOutAt(item, _item);
-                                if (conditionOne && conditionTwo)
-                                    return null
-                                else return (
-                                    <TaskListItems {...this.props} item={_item} key={_index.toString()} />
-                                )
-                            })
+                            item.data.map((_item, _index) =>
+                                <TaskListItems {...this.props} item={_item} key={_index} />
+                            )
                         }
                     </View>
                 }
             />
+            // <View></View>
         )
     }
 }
