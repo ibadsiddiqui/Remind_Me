fc29b81d3b7b017aa4666c6fa252631d3a44a53e
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-07 01:18:23 +0500

    --Commit: helper method for list filtering addedm for count and total todays task

--- src/Helpers/listHelpers.js
+++ src/Helpers/listHelpers.js
@@ -9,6 +9,14 @@ export function filterListWithFlags(list, flag) {
     return list.filter((item) => item.taskFlag === flag);
 }
 
+export function countTask(list, flag) {
+    return (list.map((item) => item.data.filter((item) => item.taskFlag === flag))).length;
+}
+
 export function sortArrayAccordingToTime(array) {
     return array.sort((a, b) => new Date(a.taskDate) - new Date(b.taskDate))
+}
+
+export function filterTodaysTask(array) {
+    return array.filter(x => new Date(x.date).toLocaleDateString() === new Date().toLocaleDateString())
 }
--- src/components/Categories/GridView/Tab/taskCountHelper.js
+++
@@ -1,3 +0,0 @@
-export function countTask(list, flag) {
-    return (list.map((item) => item.data.filter((item) => item.taskFlag === flag))).length;
-}
