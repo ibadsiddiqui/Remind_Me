ec3ba4744df72374b84f99cc48ced1a0ac79106f
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-07 22:49:15 +0500

    --Commit: Task styling updated

--- src/components/General/TaskList/styles.js
+++ src/components/General/TaskList/styles.js
@@ -6,7 +6,7 @@ const styles = {
     listContainer: { height: height * 0.8, width },
     dayHeadingContainer: { flex: 1, marginTop: 10 },
     dayHeadingText: {
-        fontSize: 13,
+        fontSize: 14,
         fontFamily: "rubik-medium",
         color: "#8B87B3",
         marginHorizontal: 10,
