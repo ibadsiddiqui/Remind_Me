931ecad5c144f5fb58f7685cac5fa9f74d9977c2
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-07 23:05:47 +0500

    --Commit: section title added for redux props

--- src/components/General/TaskListItems/index.js
+++ src/components/General/TaskListItems/index.js
@@ -7,26 +7,28 @@ import styles from './styles';
 import SwipeableRow from '../SwipeableRow';
 
 const TaskListItems = (props) => {
-    const { item, toggleTaskStatus } = props
+    const { item, toggleTaskStatus } = props;
+    const { taskFlag, taskID, completed, taskStartTime, taskDescription, taskDate } = item;
+    const sectionTitleForTaskList = new Date(taskDate).toLocaleDateString();
     return (
         <View style={styles.container}>
             <SwipeableRow {...props}>
-                <TouchableOpacity style={[Layout.tableRow, styles.styledRow(item.taskFlag)]}
-                    onPress={() => toggleTaskStatus(item.taskID)}
+                <TouchableOpacity style={[Layout.tableRow, styles.styledRow(taskFlag)]}
+                    onPress={() => toggleTaskStatus(sectionTitleForTaskList, taskID)}
                 >
                     <View style={styles.iconContainer}>
                         {
-                            item.completed ?
+                            completed ?
                                 <MaterialIcons name="radio-button-checked" color={Colors.onboardingBtnBackground} size={20} />
                                 :
                                 <MaterialIcons name="radio-button-unchecked" color={Colors.uncheckedCirlce} size={20} />
                         }
                     </View>
                     <View style={styles.timeDateContainer}>
-                        <Text style={styles.timeDateTxt}> {item.taskStartTime}</Text>
+                        <Text style={styles.timeDateTxt}> {taskStartTime}</Text>
                     </View>
                     <View style={[Layout.tableCell, { justifyContent: 'center' }]}>
-                        <Text style={styles.headingTxt(item.completed)}>{item.taskDescription}</Text>
+                        <Text style={styles.headingTxt(completed)}>{taskDescription}</Text>
                     </View>
                 </TouchableOpacity>
             </SwipeableRow>
