17190c2827dba9b67099196c580acaac1c34abd5
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-08 16:14:41 +0500

    --Commit: unneccesary code removed

--- src/constants/persistConfig.js
+++
@@ -1,7 +0,0 @@
-// defaults to localStorage AsyncStorage for react-native
-import storage from "redux-persist/lib/storage";
-
-export const persistConfigForTaskList = {
-    key: 'TaskList',
-    storage,
-}
