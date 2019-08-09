c40d806785c6e08f5db242a3997bb67294538554
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-09 22:29:15 +0500

    --Commit: username action added

--- src/redux/actions/UserActions.js
+++ src/redux/actions/UserActions.js
@@ -1,5 +1,11 @@
-import { SET_PROFILE_IMAGE } from "../../constants/Types";
+import { SET_PROFILE_IMAGE, SET_USERNAME } from "../../constants/Types";
 
+export const setUsername = (name) => {
+    return {
+        type: SET_USERNAME,
+        payload: name
+    }
+}
 export const setProfileImage = (image) => {
     return {
         type: SET_PROFILE_IMAGE,
