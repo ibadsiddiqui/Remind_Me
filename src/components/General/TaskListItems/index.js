d88b92346df0fbc8198fa741fe10608f78543028
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-10 09:14:07 +0500

    --Commit: Touch removed

--- src/components/General/TaskListItems/index.js
+++ src/components/General/TaskListItems/index.js
@@ -1,6 +1,6 @@
 import { MaterialIcons } from '@expo/vector-icons';
 import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View, TouchableHighlight } from 'react-native';
 import Colors from '../../../constants/Colors';
 import Layout from '../../../constants/Layout';
 import { getLocaleDateString, hour24Converter, getHours, getMinute } from '../../../helpers/timeConverter';
@@ -8,31 +8,36 @@ import SwipeableRow from '../SwipeableRow';
 import styles from './styles';
 
 const TaskListItems = (props) => {
-    const { item, toggleTaskStatus } = props;
+    const { item, toggleTaskStatus, navigation } = props;
     const { taskFlag, taskID, completed, taskDescription, taskDate } = item;
     const sectionTitleForTaskList = getLocaleDateString(taskDate);
     const time12 = hour24Converter(getHours(taskDate), getMinute(taskDate));
     return (
         <View style={styles.container}>
             <SwipeableRow {...props}>
-                <TouchableOpacity style={[Layout.tableRow, styles.styledRow(taskFlag)]}
-                    onPress={() => toggleTaskStatus(sectionTitleForTaskList, taskID)}
+                <TouchableHighlight style={[Layout.tableRow, styles.styledRow(taskFlag)]}
+                    onPress={() => navigation.navigate("EditTask", { itemDetails: props })}
+                    onLongPress={() => toggleTaskStatus(sectionTitleForTaskList, taskID)}
+                    underlayColor={Colors.transparent}
                 >
-                    <View style={styles.iconContainer}>
-                        {
-                            completed ?
-                                <MaterialIcons name="radio-button-checked" color={Colors.onboardingBtnBackground} size={20} />
-                                :
-                                <MaterialIcons name="radio-button-unchecked" color={Colors.uncheckedCirlce} size={20} />
-                        }
+                    <View style={Layout.tableRow}>
+
+                        <View style={styles.iconContainer}>
+                            {
+                                completed ?
+                                    <MaterialIcons name="radio-button-checked" color={Colors.onboardingBtnBackground} size={20} />
+                                    :
+                                    <MaterialIcons name="radio-button-unchecked" color={Colors.uncheckedCirlce} size={20} />
+                            }
+                        </View>
+                        <View style={styles.timeDateContainer}>
+                            <Text style={styles.timeDateTxt}>{time12}</Text>
+                        </View>
+                        <View style={[Layout.tableCell, { justifyContent: 'center' }]}>
+                            <Text style={styles.headingTxt(completed)}>{taskDescription}</Text>
+                        </View>
                     </View>
-                    <View style={styles.timeDateContainer}>
-                        <Text style={styles.timeDateTxt}>{time12}</Text>
-                    </View>
-                    <View style={[Layout.tableCell, { justifyContent: 'center' }]}>
-                        <Text style={styles.headingTxt(completed)}>{taskDescription}</Text>
-                    </View>
-                </TouchableOpacity>
+                </TouchableHighlight>
             </SwipeableRow>
         </View>
     )
