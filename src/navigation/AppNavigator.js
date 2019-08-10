c5a23e61c0583d3be52a2b045d714a028fa30844
ibadsiddiqui <ibadsiddiqui01@outlook.com>
2019-08-10 12:18:30 +0500

    --Commit: edit screen added

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -4,7 +4,7 @@ import { createAppContainer, createStackNavigator } from 'react-navigation';
 import OnboardingScreen from './../screens/OnboardingScreen'
 import Main from './TabNavigator';
 import IndividualCategoryScreen from '../screens/IndividualCategoryScreen';
-import EditTaskNavigator from './EditTaskNavigator';
+import EditTaskScreen from '../screens/EditTaskScreen';
 export default createAppContainer(createStackNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
@@ -12,7 +12,7 @@ export default createAppContainer(createStackNavigator({
   Onboarding: OnboardingScreen,
   Dashboard: Main,
   IndividualTaskCategory: IndividualCategoryScreen,
-  EditTask: EditTaskNavigator
+  EditTask: EditTaskScreen
 }, {
     headerMode: "none",
   })
